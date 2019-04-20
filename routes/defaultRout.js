916eace80c0cdba91901bd238695546eba2a9565
LAPTOP-AQHCQOB8\victo <4vitek@gmail.com>
2019-04-21 01:16:26 +0300

    small fix

--- routes/defaultRout.js
+++ routes/defaultRout.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const weatherController = require('../controllers/weatherController');
 
 /* GET default rout. */
-router.get('/', function(req, res, next) {
-  res.send('default!');
+router.get('/',async (req, res, next) => {
+  try{
+    const weatherData = await weatherController.getWeatherForecast(); 
+    res.status(200).json({data:weatherData});
+  } catch (e) {
+    //this will eventually be handled by the error handling middleware
+    console.log(e);
+    next(e); 
+  }
 });
 
 module.exports = router;
